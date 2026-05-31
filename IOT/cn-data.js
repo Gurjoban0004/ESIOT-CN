f8a9da7cdf36dbb5e082dc8e41a1c64792520b22
Gurjoban Singh <gurjobanpanjeta@gmail.com>
2026-05-31 22:45:11 +0530

    feat: restore original CN data and add Notes/Cheat Sheet sections

--- IOT/cn-data.js
+++ IOT/cn-data.js
@@ -1,6 +1,11 @@
 const CN_STUDY_DATA = {
-  st1: CN_ST1,
-  st2: CN_ST2,
-  endTerm: CN_ENDTERM,
-  cheatSheet: CN_CHEATSHEET
+  unit1_2: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.unit1_2 : [],
+  unit3_4: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.unit3_4 : [],
+  unit5_6: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.unit5_6 : [],
+  unit7_8: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.unit7_8 : [],
+  unit9: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.unit9 : [],
+  notes: [...CN_ST1, ...CN_ST2, ...CN_ENDTERM],
+  cheatSheet: CN_CHEATSHEET,
+  practice: typeof CN_STUDY_DATA_OLD !== 'undefined' ? CN_STUDY_DATA_OLD.practice : []
 };
+
